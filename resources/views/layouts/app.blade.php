ad4278faf5d7638498b86d7b01efc39ba750e8b3
Kamran Ali <kamran303516@gmail.com>
2026-09-15 22:09:09 +0500

    perf(lighthouse): eliminate render-blocking google fonts and expand slider dot touch targets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,10 +36,14 @@
   <!-- GOOGLE FONTS (Brand Guide 2026: Cormorant Garamond, Montserrat, Alex Brush) -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cormorant+Garamond:ital,wght@0,500;0,600;0,700;1,500;1,600&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cormorant+Garamond:ital,wght@0,500;0,600;0,700;1,500;1,600&family=Montserrat:wght@400;500;600;700&display=swap">
+  <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cormorant+Garamond:ital,wght@0,500;0,600;0,700;1,500;1,600&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+  <noscript>
+    <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Cormorant+Garamond:ital,wght@0,500;0,600;0,700;1,500;1,600&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+  </noscript>
 
   <!-- MASTER BRAND STYLESHEET -->
-  <link rel="stylesheet" href="{{ asset('css/brand.css') }}?v=20260915v5">
+  <link rel="stylesheet" href="{{ asset('css/brand.css') }}?v=20260915v6">
 
   @stack('styles')
   @yield('extra_css')
